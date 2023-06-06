7f28d78e538e4923f67b37fa1965d57603d9bedf
gaurav.kumar <gaurav.paul@outlook.in>
2023-06-06 09:57:08 +0530

    fetch data from property file

--- src/main/java/com/example/project/demo/spring/Application.java
+++ src/main/java/com/example/project/demo/spring/Application.java
@@ -6,26 +6,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.implementation")
+@PropertySource("var.properties")
 public class Application {
 
 	public static void main(String[] args) {
 		       ApplicationContext context = SpringApplication.run(Application.class, args);
-         ScopeLearn scopeLearn = context.getBean(ScopeLearn.class);
-		System.out.println(scopeLearn);
-		System.out.println(scopeLearn.getJdbcConnection());
-		ScopeLearn scopeLearn1 = context.getBean(ScopeLearn.class);
-		System.out.println(scopeLearn1);
-		System.out.println(scopeLearn1);
-		System.out.println(scopeLearn1.getJdbcConnection());
+//         ScopeLearn scopeLearn = context.getBean(ScopeLearn.class);
+//		System.out.println(scopeLearn);
+//		System.out.println(scopeLearn.getJdbcConnection());
+//		ScopeLearn scopeLearn1 = context.getBean(ScopeLearn.class);
+//		System.out.println(scopeLearn1);
+//		System.out.println(scopeLearn1);
+//		System.out.println(scopeLearn1.getJdbcConnection());
 
-					   BinarySearch search = context.getBean(BinarySearch.class);
-		System.out.print(search.logic());
+		BinarySearch search = context.getBean(BinarySearch.class);
+		System.out.println(search.logic());
 		System.out.println(search);
-		BinarySearch search1 = context.getBean(BinarySearch.class);
-		System.out.println(search1);
+//		BinarySearch search1 = context.getBean(BinarySearch.class);
+//		System.out.println(search1);
 
 	}
 
--- src/main/java/com/implementation/businessLogic/BinarySearch.java
+++ src/main/java/com/implementation/businessLogic/BinarySearch.java
@@ -2,6 +2,7 @@ package com.implementation.businessLogic;
 import com.interfaces.Sorting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class BinarySearch {
-
     @Autowired
     @Qualifier("bubble")
     Sorting bubbleSort;
 
+    @Value("${name}")
+    public String name;
+
     public int logic() {
+        System.out.println(name);
         return bubbleSort.sort(new int[] {23,54,56,76});
     }
 }
